87b17ff439498e1e2128779225f580d530c50a8f
Ryan Andri <ryanandri@linuxmail.org>
2018-12-13 16:41:47 +0700

    santoni: rootdir: enable process reclaim memory
    * enabled it after parameters configured to avoid race condition.

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -67,31 +67,29 @@ function configure_memory_parameters() {
     set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
     echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
     echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-    #echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-    #echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-    #echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
-    #echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+    echo 70 > /sys/module/process_reclaim/parameters/pressure_max
+    echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
     if [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 2097152 ]; then
-        #echo 10 > /sys/module/process_reclaim/parameters/pressure_min
-        #echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
+        echo 10 > /sys/module/process_reclaim/parameters/pressure_min
+        echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
         adjZeroMinFree=18432
     elif [ "$arch_type" == "aarch64" ] && [ $MemTotal -gt 1048576 ]; then
-        #echo 10 > /sys/module/process_reclaim/parameters/pressure_min
-        #echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
+        echo 10 > /sys/module/process_reclaim/parameters/pressure_min
+        echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
         adjZeroMinFree=14746
     elif [ "$arch_type" == "aarch64" ]; then
-        #echo 50 > /sys/module/process_reclaim/parameters/pressure_min
-        #echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
+        echo 50 > /sys/module/process_reclaim/parameters/pressure_min
+        echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
         adjZeroMinFree=14746
     else
-        #echo 50 > /sys/module/process_reclaim/parameters/pressure_min
-        #echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
+        echo 50 > /sys/module/process_reclaim/parameters/pressure_min
+        echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
         echo "15360,19200,23040,26880,34415,43737" > /sys/module/lowmemorykiller/parameters/minfree
         echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
         adjZeroMinFree=15360
@@ -100,7 +98,7 @@ function configure_memory_parameters() {
     echo $clearPercent > /sys/module/zcache/parameters/clear_percent
     echo 30 >  /sys/module/zcache/parameters/max_pool_percent
 
-    # Enabling Adaptive Lowmemorykiller
+    echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
     echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 
     if [ -f /sys/devices/soc0/soc_id ]; then
