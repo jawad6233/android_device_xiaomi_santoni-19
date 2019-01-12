9a87209873b0dbe9d9c589d21bca8a198ca3c8a4
Ryan Andri <ryanandri@linuxmail.org>
2019-01-12 19:00:42 +0700

    santoni: sepolicy: import from Lineage sepolicy [DNM]
    * from:
      https://github.com/LineageOS/android_device_xiaomi_santoni
      https://github.com/LineageOS/android_device_xiaomi_msm8937-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -223,9 +223,9 @@ TARGET_RIL_VARIANT := caf
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_VERS := 28.0
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
